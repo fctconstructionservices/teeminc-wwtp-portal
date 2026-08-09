6135ef6f8961038cc0d1d4dfb8bc20fcde17006a
Darwin Fabon <f2cconstructionservice@gmail.com>
2026-08-09 22:59:48 +0800

    fixing settings

--- js/core/config.js
+++ js/core/config.js
@@ -45,7 +45,7 @@ const CONFIG = {
      * apiUrl - the Apps Script Web App /exec URL for THIS company.
      * Each company has its own deployment reading its own spreadsheet.
      */
-    apiUrl: 'https://script.google.com/macros/s/AKfycbwoInhyBINQwtEw66Dh94xT1q6L9aC225NtN86FSygY1PiaUbfBTPhciShCor-puMuc/exec',
+    apiUrl: 'https://script.google.com/macros/s/AKfycbyIuNjph0XC0Jd4yc777cOXrBnzt1rtv8rvzI8X0WISEtuz31CBJmTngv9c7Wcte1Xp_g/exec',
 
     /** company - what the app calls itself. */
     company: {
